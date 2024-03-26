cb48af0d0a76ec4a71b76feebf28b3bf3c811fe6
Grimaldi-Joao <jaoagrimaldi6@gmail.com>
2024-03-25 23:49:55 -0300

    Criacão

---
+++ src/main/java/com/enge_softwere/Calculator.java
@@ -0,0 +1,7 @@
+package com.enge_softwere;
+
+public class Calculator {
+    public int add(int a, int b) {
+        return a + b;
+    }
+}
---
+++ src/test/java/com/enge_softwere/CalculatorTest.java
@@ -0,0 +1,25 @@
+package com.enge_softwere;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class CalculatorTest 
+{
+    @Test
+    public void testAdd() {
+        Calculator calculator = new Calculator();
+        
+        // Teste 1
+        assertEquals(5, calculator.add(2, 3));
+        
+        // Teste 2
+        assertEquals(10, calculator.add(5, 5));
+        
+        // Teste 3
+        assertEquals(0, calculator.add(-5, 5));
+    }
+}
