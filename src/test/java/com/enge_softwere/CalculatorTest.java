0326762a25a4743ff692ddd5db48a72745fc3c8f
Grimaldi-Joao <jaoagrimaldi6@gmail.com>
2024-03-26 09:52:38 -0300

    Test

--- src/test/java/com/enge_softwere/CalculatorTest.java
+++ src/test/java/com/enge_softwere/CalculatorTest.java
@@ -14,7 +14,7 @@ public class CalculatorTest
         Calculator calculator = new Calculator();
         
         // Teste 1
-        assertEquals(6, calculator.add(2, 3));
+        assertEquals(5, calculator.add(2, 3));
         
         // Teste 2
         assertEquals(10, calculator.add(5, 5));
